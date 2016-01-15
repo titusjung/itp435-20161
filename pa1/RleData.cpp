a614f04dff9cb39ab2c27db98b9749a226115de1
Titus Jung <titusjung@gmail.com>
2016-01-15 15:38:59 -0800

    started on compression algorithm

--- pa1/RleData.cpp
+++ pa1/RleData.cpp
@@ -3,6 +3,26 @@
 void RleData::Compress(const char* input, size_t inSize)
 {
 	// TODO
+	char*  temp = new char[inSize];
+
+	int charPos=1;
+	int numPos=0; 
+	char currChar; 
+	char prevChar = input[numPos];
+	for (int i = 1; i < inSize; i++)
+	{
+		currChar = input[i]; 
+		if (currChar == prevChar)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+
+	delete[] temp; 
 }
 
 void RleData::Decompress(const char* input, size_t inSize, size_t outSize)
